52634804e7574bad36085006354c0128baf1ab78
Juraj Paluba <juraj.palubajp@gmail.com>
2025-03-05 21:17:48 +0100

    feat(backend-rs): create save file endpoint
    I have created .http file for testing purposes. However, I was not able
    to find a client that can execute .http file with multipart request from
    NeoVim or terminal.

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,13 +1,36 @@
-use actix_web::{post, App, HttpResponse, HttpServer, Responder};
+use std::io::Read;
+
+use actix_multipart::form::{tempfile::TempFile, MultipartForm};
+use actix_web::{post, App, Error, HttpResponse, HttpServer, Responder};
 
 #[post("/health")]
 async fn echo() -> impl Responder {
     HttpResponse::Ok().body("healthy")
 }
 
+#[derive(Debug, MultipartForm)]
+struct UploadForm {
+    #[multipart(rename = "file")]
+    files: Vec<TempFile>,
+}
+
+#[post("/files")]
+async fn save_files(
+    MultipartForm(form): MultipartForm<UploadForm>,
+) -> Result<impl Responder, Error> {
+    println!("{:?}", form);
+    for mut f in form.files {
+        let mut file_content = vec![];
+        f.file.read_to_end(&mut file_content).unwrap();
+        println!("{:?}", std::str::from_utf8(&file_content).unwrap())
+    }
+
+    Ok(HttpResponse::Ok())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().service(echo))
+    HttpServer::new(move || App::new().service(echo).service(save_files))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
