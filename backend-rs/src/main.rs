87ec7a7804bf02528ac64f38b80292d17114cbdc
Juraj Paluba <juraj.palubajp@gmail.com>
2025-03-08 23:32:57 +0100

    feat(backend-rs): create load bedgraph endpoint
    Upon loading the file (we should save the bedgraph files to some temp
    storage or nonsql??) it parses out graph relevant (at least for now)
    parts and returns them to create a simple line graph.
    
    FYI, using buffer is much faster than creating String for each line.
    
    I have updated the example bedgraph that I have downloaded from
    https://bioinformatics.ibp.cz/. I hope that other bedgraphs will have
    the same format (first two lines are metadata and graph relevant data
    are second and last column).

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,8 +1,10 @@
-use std::io::Read;
+use std::io::{self, BufRead};
 
 use actix_cors::Cors;
 use actix_multipart::form::{tempfile::TempFile, MultipartForm};
+use actix_web::http::header::ContentType;
 use actix_web::{post, App, Error, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
 
 #[post("/health")]
 async fn echo() -> impl Responder {
@@ -15,18 +17,37 @@ struct UploadForm {
     files: Vec<TempFile>,
 }
 
+#[derive(Serialize)]
+struct BedgraphDataPoints {
+    x: Vec<i32>,
+    y: Vec<i32>,
+}
+
 #[post("/files")]
 async fn save_files(
     MultipartForm(form): MultipartForm<UploadForm>,
 ) -> Result<impl Responder, Error> {
-    println!("{:?}", form);
-    for mut f in form.files {
-        let mut file_content = vec![];
-        f.file.read_to_end(&mut file_content).unwrap();
-        println!("{:?}", std::str::from_utf8(&file_content).unwrap())
+    let mut file_buffer;
+    let file = form.files.first().unwrap();
+    file_buffer = io::BufReader::new(&file.file).lines();
+
+    // skip frist 2 lines
+    file_buffer.next();
+    file_buffer.next();
+
+    let mut x = vec![];
+    let mut y = vec![];
+    for line in file_buffer.map_while(Result::ok) {
+        let mut parts = line.split(" ");
+        x.push(parts.nth(1).unwrap().parse::<i32>().unwrap());
+        y.push(parts.last().unwrap().parse::<i32>().unwrap());
     }
 
-    Ok(HttpResponse::Ok())
+    let response = serde_json::to_string(&BedgraphDataPoints { x, y }).unwrap();
+
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(response))
 }
 
 #[actix_web::main]
